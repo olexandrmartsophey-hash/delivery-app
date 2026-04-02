2485e3144bcb9a5118bddcc2f979dcac126ba44a
olexandrmartsophey-hash <olexandrmartsophey@gmail.com>
2026-04-02 18:57:21 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/delivery');
+mongoose.connect(process.env.MONGO_URL);
 
 app.use('/shops', require('./routes/shops'));
 app.use('/products', require('./routes/products'));
